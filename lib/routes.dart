3912c977860a22f09696d5ab3193a132ebdf8020
Quang Dong <quangdong211@gmail.com>
2020-08-20 11:07:33 +0700

    home screen

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:trung_son/screens/home/home_screen.dart';
 import 'package:trung_son/screens/sign_in/sign_in_screen.dart';
 import 'package:trung_son/screens/splash/splash_screen.dart';
 
 final Map<String, WidgetBuilder> routes = {
   SplashScreen.routeName: (content) => SplashScreen(),
   SignInScreen.routeName: (content) => SignInScreen(),
+  HomeScreen.routeName: (content) => HomeScreen(),
 };
